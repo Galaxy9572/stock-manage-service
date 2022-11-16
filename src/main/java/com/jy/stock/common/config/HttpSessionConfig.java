66b21ea4d9aeb1ed51aae967238454687619c6e3
JY <ljy565880512@qq.com>
2022-11-16 11:38:09 +0800

    引入mongodb

--- src/main/java/com/jy/stock/common/config/HttpSessionConfig.java
+++ src/main/java/com/jy/stock/common/config/HttpSessionConfig.java
@@ -1,13 +1,21 @@
 package com.jy.stock.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
 /**
  * Http Session 配置
  * @author liaojunyao
  */
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+@EnableMongoHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class HttpSessionConfig {
+
+    @Bean
+    public JacksonMongoSessionConverter jacksonMongoSessionConverter() {
+        return new JacksonMongoSessionConverter();
+    }
+
 }
